69b79ededbc523d07080839c267d8f2c543c67ff
Drew Loveridge <drew.loveridge@makemarks.com>
2022-02-18 19:09:53 -0500

    added Lego preset Button

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import subprocess
 import os
 import sys
-from PySide2 import QtCore,QtWidgets,QtGui,QtUiTools
+from pathlib import Path
+
+
+_this_dir = Path(os.path.dirname(__file__))
+os.sys.path.append(str(_this_dir))
+
+# Code Path Collection
+_code_base_path = _this_dir.parent
+_code_base_name = _code_base_path.name
+_ui_folder_path = _this_dir.joinpath("UI")
+_site_pak_path  = _code_base_path.joinpath("Python3","Global_Systems","AW_site_packages")
+
+
+
+os.sys.path.append(str(_site_pak_path))
+
+from PySide2 import QtCore, QtGui, QtUiTools, QtWidgets
+QtSlot         = QtCore.Slot
+QtSignal       = QtCore.Signal
+QtProperty     = QtCore.Property
+
 import icons
 
-_this_dir = os.path.dirname(__file__)
-_code_base_path = os.path.realpath(_this_dir+"/..")
-_code_base_name = os.path.split(_code_base_path)[-1]
-_ui_folder_path = os.path.join(_this_dir, "UI")
-
 _UI_Loader = QtUiTools.QUiLoader()
+os.sys.path.remove(str(_site_pak_path))
 
 ########################################################################
 class Modes:
@@ -21,10 +37,19 @@ class Modes:
 
 
 
+
+#----------------------------------------------------------------------
+def get_Maya_Versions():
+	""""""
+	res = []
+	p = Path("C:/Program Files/Autodesk")
+	for item in p.glob("Maya*/bin/maya.exe"):
+		res.append(item.parent.parent.name.replace("Maya",""))
+	return res
 #----------------------------------------------------------------------
 def Maya_Enable_Legacy_Render_Layers(val):
 	"""Turns on of off the use of MAYA_ENABLE_LEGACY_RENDER_LAYERS"""
-	if val == True and not "MAYA_ENABLE_LEGACY_RENDER_LAYERS" in os.environ:
+	if val == True:
 		os.environ["MAYA_ENABLE_LEGACY_RENDER_LAYERS"] = "1"
 	elif "MAYA_ENABLE_LEGACY_RENDER_LAYERS" in os.environ:
 		del os.environ["MAYA_ENABLE_LEGACY_RENDER_LAYERS"] 
@@ -56,11 +81,13 @@ def Remove_Studio_Code_From_Python_Path():
 	""""""
 	if "PYTHONPATH" in os.environ:
 		new_python_path = []
-	for old_path_item in os.environ["PYTHONPATH"].split(";"):
-		if not _code_base_name in old_path_item and not "Git_Live_Code" in old_path_item:
-			new_python_path.append(old_path_item)
-	os.environ["PYTHONPATH"] = ";".join(new_python_path)
-
+		
+		for old_path_item in os.environ["PYTHONPATH"].split(";"):
+			if not _code_base_name in old_path_item and not "Git_Live_Code" in old_path_item:
+				new_python_path.append(old_path_item)
+				
+		os.environ["PYTHONPATH"] = ";".join(new_python_path)
+		
 #----------------------------------------------------------------------
 def Remove_Studio_Code_From_Maya_Script_Path():
 	""""""
@@ -74,11 +101,16 @@ def Remove_Studio_Code_From_Maya_Script_Path():
 #----------------------------------------------------------------------
 def Remove_Studio_Code_From_System_Path():
 	""""""
-	new_python_path = []
-	for old_path_item in os.environ["PATH"].split(";"):
-		if not _code_base_name in old_path_item and not "Git_Live_Code" in old_path_item:
-			new_python_path.append(old_path_item)
-	os.environ["PATH"] = ";".join(new_python_path)
+	if "PATH" in os.environ["PATH"]:
+		current_sys_paths = os.environ["PATH"].split(";")
+		new_sys_path = []
+		
+		for old_path_item in current_sys_paths:
+			
+			if not _code_base_name in old_path_item and not "Git_Live_Code" in old_path_item:
+				new_sys_path.append(old_path_item)
+				
+		os.environ["PATH"] = ";".join(new_sys_path)
 
 #----------------------------------------------------------------------
 def Setup_Maya_For_Clean():
@@ -87,6 +119,51 @@ def Setup_Maya_For_Clean():
 	Remove_Studio_Code_From_Maya_Script_Path()
 	Remove_Studio_Code_From_System_Path()
 	
+
+#----------------------------------------------------------------------
+def Add_Maya_Module_Path(path):
+	""""""
+	path = str(path)
+	if not "MAYA_MODULE_PATH" in os.environ:
+		os.environ["MAYA_MODULE_PATH"] = path
+	else:
+		current_paths = os.environ["MAYA_MODULE_PATH"].split(";")
+		if not path in current_paths:
+			current_paths.append(path)
+		os.environ["MAYA_MODULE_PATH"] = ";".join( current_paths )
+#----------------------------------------------------------------------
+def Remove_Maya_Module_Path(path):
+	""""""
+	path = str(path)
+	if "MAYA_MODULE_PATH" in os.environ:
+		current_paths = os.environ["MAYA_MODULE_PATH"].split(";")
+		if path in current_paths:
+			current_paths.remove(path)
+		os.environ["MAYA_MODULE_PATH"] = ";".join(current_paths)
+
+#----------------------------------------------------------------------
+def Add_Path_To_Python_Path(path):
+	""""""
+	path = str(path)
+	if not "PYTHONPATH" in os.environ:
+		os.environ["PYTHONPATH"] = path
+	else:
+		current_paths = os.environ["PYTHONPATH"].split(";")
+		
+		if not path in current_paths:
+			current_paths.append(path)
+			
+		os.environ["PYTHONPATH"] = ";".join( current_paths )
+#----------------------------------------------------------------------
+def Remove_Path_From_Python_Path(path):
+	""""""
+	path = str(path)
+	if "PYTHONPATH" in os.environ:
+		current_paths = os.environ["PYTHONPATH"].split(";")
+		if path in current_paths:
+			current_paths.remove(path)
+		os.environ["PYTHONPATH"] = ";".join(current_paths)
+		
 #----------------------------------------------------------------------
 def Setup_Maya_For_Studio(python_version):
 	""""""
@@ -125,23 +202,40 @@ class Software_Launcher_UI(QtWidgets.QWidget):
 			self.legacyRenderLayersCheckBox = QtWidgets.QCheckBox()
 			self.legacyViewportCheckBox = QtWidgets.QCheckBox()
 			self.userToolsCheckBox = QtWidgets.QCheckBox()
-
+			
 			self.versionComboBox = QtWidgets.QComboBox()
 			self.modeComboBox    = QtWidgets.QComboBox()
 			self.pythonComboBox  = QtWidgets.QComboBox()
 			self.LaunchButton   = QtWidgets.QPushButton()
-			self.LaunchButton.clicked
-			self.versionComboBox.currentIndexChanged
+			self.AutomotiveButton = QtWidgets.QComboBox()
+			self.CleanButton = QtWidgets.QComboBox()
+			self.LegoButton = QtWidgets.QComboBox()
+	
 	#----------------------------------------------------------------------
-	@QtCore.Slot()
-	def on_versionComboBox_currentIndexChanged(self):
+	def _init(self):
 		""""""
-		if self.versionComboBox.currentText() != "2022":
-			self.pythonComboBox.setCurrentText("2")
+		self.versionComboBox.clear()
+		maya_versions = get_Maya_Versions()
+		maya_versions.reverse()
+		self.versionComboBox.addItems(maya_versions)		
+	#----------------------------------------------------------------------
+	@QtCore.Slot(str)
+	def on_versionComboBox_currentIndexChanged(self,val):
+		""""""
+		val
+		if int(self.versionComboBox.currentText()) < 2022:
 			self.pythonComboBox.setDisabled(True)
 		else:
-			self.pythonComboBox.setCurrentText("3")
 			self.pythonComboBox.setDisabled(False)
+	#----------------------------------------------------------------------
+	@QtCore.Slot(str)
+	def on_modeComboBox_currentIndexChanged(self,val):
+		""""""
+		val
+		if self.modeComboBox.currentText() == Modes.Clean:
+			self.userToolsCheckBox.setDisabled(True)
+		else:
+			self.userToolsCheckBox.setDisabled(False)
 	#----------------------------------------------------------------------
 	@QtCore.Slot()
 	def on_LaunchButton_clicked(self):
@@ -167,6 +261,41 @@ class Software_Launcher_UI(QtWidgets.QWidget):
 		
 		cmd = Build_Launch_Command(maya_version)
 		subprocess.Popen(cmd)
+
+	#----------------------------------------------------------------------
+	@QtCore.Slot()
+	def on_AutomotiveButton_clicked(self):
+		""""""
+		Setup_Maya_For_Studio("2")
+		Maya_Enable_Legacy_Render_Layers(True)
+		Maya_Enable_Legacy_Viewport(True)
+		Enable_User_Tools(True)
+		cmd = Build_Launch_Command("2020")
+		subprocess.Popen(cmd)
+		
+	#----------------------------------------------------------------------
+	@QtCore.Slot()
+	def on_CleanButton_clicked(self):
+		""""""
+		Setup_Maya_For_Clean()
+		Set_Maya_Python_Version("2")
+		Maya_Enable_Legacy_Render_Layers(False)
+		Maya_Enable_Legacy_Viewport(True)
+		Enable_User_Tools(False)
+		cmd = Build_Launch_Command("2020")
+		subprocess.Popen(cmd)
+	#----------------------------------------------------------------------
+	@QtCore.Slot()
+	def on_LegoButton_clicked(self):
+		""""""
+		Setup_Maya_For_Clean()
+		Set_Maya_Python_Version("3")
+		Maya_Enable_Legacy_Render_Layers(False)
+		Maya_Enable_Legacy_Viewport(True)
+		Enable_User_Tools(False)
+		Add_Maya_Module_Path( _code_base_path.joinpath("_3rd_Party","LLRToolset","Tools_2022") )
+		cmd = Build_Launch_Command("2022")
+		subprocess.Popen(cmd)
 		
 _UI_Loader.registerCustomWidget(Software_Launcher_UI)
 
@@ -180,10 +309,8 @@ def make_ui():
 if __name__ == '__main__':
 	app = QtWidgets.QApplication(sys.argv)
 	win = make_ui()
+	win._init()
 	QtCore.QMetaObject.connectSlotsByName(win)
+	win.setWindowFlags(QtCore.Qt.WindowTitleHint)
 	win.show()
 	sys.exit(app.exec_())
-
-
-
-subprocess.call(r'"c:\Program Files\Autodesk\Maya2018\bin\maya.exe"')
